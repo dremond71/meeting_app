d83f86bbe42ecbb010eb12d5161e7eede1d40b6d
dremond71 <dominique.emond@gmail.com>
2021-02-19 21:40:09 -0500

    chore: chat part 1

--- public/components/ChatArea.js
+++ public/components/ChatArea.js
@@ -1,14 +1,14 @@
-import ParticipantChat2 from './ParticipantChat2.js';
+import ParticipantChat from './ParticipantChat.js';
 
 export default {
   name: 'ChatArea',
   components: {
-    ParticipantChat2,
+    ParticipantChat,
   },
   template: `
 <div v-if="show" class="w3-center w3-container w3-border w3-round-large w3-white" style="margin-top:10px;margin-right:10px;">
-  <ParticipantChat2></ParticipantChat2>
-  <div class="w3-white" style="height:80px;"></div>
+  <ParticipantChat></ParticipantChat>
+  <div class="w3-white" style="height:80px;margin-bottom:10px;"></div>
 </div>`,
   computed: {
     show() {
--- public/components/ParticipantChat.js
+++ public/components/ParticipantChat.js
@@ -2,39 +2,43 @@ export default {
   name: 'ParticipantChat',
   components: {},
   template: `
-<div class="w3-container  w3-border w3-round-large w3-margin-top">
-    <div  class="w3-panel w3-blue w3-round-large" v-on:click="toggle">{{ name }}</div>
-    <div id="Demo1" v-bind:class="sectionClasses">
-    The contents are for {{ name }}. Chat not fully implemented yet.
-    </div>
-</div>`,
-  props: ['connectedItem'],
+  <div class="w3-container w3-margin-top" style="width:100%">
+    <h3>Chat</h3>
+    <label for="receivedText" class="w3-margin-top"><b>Messages</b></label>
+    <textarea id="receivedText" name="receivedText" rows="8" style="width:100%;resize:none;" class="w3-margin-bottom;" readonly>
+    </textarea>
+    <hr/>
+    <label for="selectNames"><b>Send Message To:</b></label>
+    <select name="selectNames" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" style="width:100%">
+        <option v-for="item in connectedItems" v-bind:key="item.id"   v-bind:value="item.id" >{{item.value}}</option>     
+    </select>
+    <textarea id="sendText" name="sendText" rows="6"  class="w3-margin-top" style="width:100%;resize:none;">
+    </textarea>
+    <button class="w3-button w3-round-xxlarge w3-light-blue" style="width:100%;">Send</button>
+  </div>`,
   data: function () {
     return {
       open: false,
     };
   },
   computed: {
-    name() {
-      let theName = this.connectedItem.userName;
-      // if user name is undefined or empty string, use id value.
-      if (!theName || theName.trim() === '') {
-        theName = this.connectedItem.id;
+    connectedItems() {
+      const listItems = [];
+      const everyoneItem = {
+        id: 'everyone',
+        chatId: 'chat_everyone',
+        value: 'Everyone',
+      };
+      listItems.push(everyoneItem);
+      let items = this.$store.getters.everyoneButMe;
+      for (const item of items) {
+        listItems.push({
+          id: item.id,
+          chatId: `chat_${item.id}`,
+          value: item.userName,
+        });
       }
-      console.log(`The name of participant is ${theName}`);
-      return theName;
-    },
-    sectionClasses() {
-      let someClasses = 'w3-container w3-border w3-margin-bottom';
-      if (!this.open) {
-        someClasses += ' w3-hide';
-      }
-      return someClasses;
-    },
-  },
-  methods: {
-    toggle() {
-      this.open = !this.open;
+      return listItems;
     },
   },
 };
