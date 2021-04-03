64db5cbe4fe0b0f8e94462120fa01267e09eba00
dremond71 <dominique.emond@gmail.com>
2021-04-02 22:12:39 -0400

    feat: add chat notifier in top right corner

---
+++ public/components/ChatNotifier.js
@@ -0,0 +1,44 @@
+export default {
+  name: 'ChatNotifier',
+  components: {},
+  template: `
+  <div v-if="thereAreMessages" v-bind:class="theClasses">
+      Chat Messages: {{chatMessageCount}}
+  </div>   
+  `,
+  data: function () {
+    return {
+      notifyColor: 'w3-black',
+      number: 0,
+      processingChange: false,
+    };
+  },
+  computed: {
+    theClasses() {
+      return `w3-bar-item w3-right ${this.notifyColor}`;
+    },
+    thereAreMessages() {
+      return this.chatMessageCount > 0;
+    },
+    chatMessageCount() {
+      const latestCount = this.$store.getters.allChatMessages.length;
+
+      // if a new message came in, turn background yellow
+      // for 2 seconds to notify the user visually
+      // (over and above the notification sound)
+      if (this.number !== latestCount) {
+        if (!this.processingChange) {
+          this.processingChange = true;
+          this.notifyColor = 'w3-yellow';
+          setTimeout(() => {
+            this.number = this.$store.getters.allChatMessages.length;
+            this.notifyColor = 'w3-black';
+            this.processingChange = false;
+          }, 2000);
+        }
+      }
+
+      return latestCount;
+    },
+  },
+};
--- public/components/NavBar.js
+++ public/components/NavBar.js
@@ -1,14 +1,18 @@
 import Clock from './Clock.js';
 import ConnectedBadge from './ConnectedBadge.js';
+import ChatNotifier from './ChatNotifier.js';
+
 export default {
   name: 'NavBar',
   components: {
     Clock,
+    ChatNotifier,
     ConnectedBadge,
   },
   template: `
     <div class="w3-bar w3-black">
       <Clock></Clock>
+      <ChatNotifier></ChatNotifier>
       <ConnectedBadge></ConnectedBadge>
     </div>`,
 };
